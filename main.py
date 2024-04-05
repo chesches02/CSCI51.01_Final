e650f1371f869c26acb82bd14705ef4e719a8bfc
NeptuneReal <38521285+Neptuneee11@users.noreply.github.com>
2024-04-05 12:05:04 +0800

    user inputs added

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-process = int(input("how many processes would you like to simulate"))
 
-arrival_time = []
+class process():
+    def __init__(self, turnaroundTime, arrivalTime):
+        self.turnaroundTime = turnaroundTime
+        self.arrivalTime = arrivalTime
 
-TurnaroundTime = []
+processNum = int(input("how many processes would you like to simulate?: "))
 
-for i in process:
-    arrival_time[i] = int(input("Indicate arrival time of process"+i+":"))
-for i in process:
-    TurnaroundTime[i] = int(input("Indicate Turnaround time of process"+i+":"))
-algorithm = (input("Which Scheduling algorithm would you like to simulate,type \
-          either SJF, Preemptive, or Round Robin only. It's case sensitive"))
-if algorithm == "SJF":
+processList = []
+
+#inputting each process
+for i in range(processNum):
+    arrival_time = int(input("Indicate arrival time of process "+str(i+1)+" (ns): "))
+    turnaround_time = int(input("Indicate Turnaround time of process "+str(i+1)+" (ns): "))
+    p = process(turnaround_time, arrival_time)
+    processList.append(p)
+    print("===========================================")
+
+print("Processes")
+for i in range(processNum):
+    print("Process "+str(i+1)+" A_t: "+ str(processList[i].arrivalTime)+" T_t: "+str(processList[i].turnaroundTime))
+print("======================================")
+algorithm = (input("Which Scheduling algorithm would you like to simulate,type \n either SJF, Preemptive, or Round Robin only."))
+algorithm.lower
+if algorithm == "sjf":
     algorithm = "do something"
-elif algorithm == "Preemptive":
+elif algorithm == "preemptive":
     algorithm = "do something"
-elif algorithm == "RoundRobin":
+elif (algorithm == "round robin" or algorithm == "roundrobin"):
     algorithm = "do something"
