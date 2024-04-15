75e06ea0d4365fbef67506d9631c4c807f913ce0
NeptuneReal <38521285+Neptuneee11@users.noreply.github.com>
2024-04-15 20:21:00 +0800

    works improperly

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 
 class process():
     waitingTime = 0
@@ -16,16 +17,14 @@ processList = []
 for i in range(processNum):
     arrival_time = int(input("Indicate arrival time of process "+str(i+1)+" (ns): "))
     turnaround_time = int(input("Indicate Turnaround time of process "+str(i+1)+" (ns): "))
-    p = process(turnaround_time, arrival_time)
+    p = process(i, turnaround_time, arrival_time)
     processList.append(p)
     print("===========================================")
 
 print("Processes")
 for i in range(processNum):
-    print("Process "+str(i+1)+" A_t: "+ str(processList[i].arrivalTime)+" T_t: "+str(processList[i].turnaroundTime))
+    print("Process "+str(processList[i].id)+" A_t: "+ str(processList[i].arrivalTime)+" T_t: "+str(processList[i].turnaroundTime))
 print("======================================")
-algorithm = (input("Which Scheduling algorithm would you like to simulate,type \n either SJF, Preemptive, or Round Robin only."))
-algorithm.lower
 
 def mergeSort_arrivalTime(proc):
     if len(proc) > 1:
@@ -70,13 +69,85 @@ def mergeSort_arrivalTime(proc):
     return proc
 
 def roundRobin(pList, t_q):
-    pass
+
+    print("Processes sorted by arrival time")
+    for i in range(processNum):
+        print("Process "+str(processList[i].id)+" A_t: "+ str(pList[i].arrivalTime)+" T_t: "+str(pList[i].turnaroundTime))
+    print("======================================")
+
+    processesLeft = len(pList)
+    timeLeft = []
+    processIndex = 0
+    currentProcessPointer = 0
+    rrq = []
+    hasZero = False
+    ns = 0
+    runningFor = 0
+
+    while(processesLeft > 0):
+        print(ns)
+
+        try:
+            #take all processes that arrive at this nanosecond and add them to the process queue
+            while(pList[processIndex].arrivalTime == ns):
+                arrivingProc = pList[processIndex]
+                print("     Process "+ str(arrivingProc.id) + " has arrived")
+                rrq.append(arrivingProc)
+                timeLeft.append(arrivingProc.turnaroundTime)
+                processIndex += 1
+        except:
+            pass
+
+        #handles the process queue
+        if len(rrq) > 0:
+            print("runningfor: " + str(runningFor))
+            # handle processpointer
+            if runningFor == (t_q):
+                print("     context switch")
+                currentProcessPointer += 1
+                currentProcessPointer %= len(rrq)
+                runningFor = 1
+            else:
+                runningFor += 1
+
+            hasZero = False
+            # Iterates through every process and either deducts its turnaroundTime or adds its waitingTime
+            for i in range(len(rrq)):
+                if i == currentProcessPointer:
+                    timeLeft[i] -= 1
+                    print("     Process "+ str(rrq[i].id) + " ran for 1 ns with " + str(timeLeft[i]) + " ns left")
+                    if timeLeft[i] == 0:
+                        hasZero = True
+                else:
+                    print("     Process "+ str(rrq[i].id) + " waited for 1 ns")
+                    pList[currentProcessPointer].waitingTime += 1
+            
+            # find where the zero is when detected
+            if hasZero:
+                i = 0
+                while i < len(rrq):
+                    if timeLeft[i] == 0:
+                        rrq.pop(i)
+                        timeLeft.pop(i)
+                        print("     context switch early finish")
+                        processesLeft -= 1
+                        runningFor = 1
+                    else:
+                        i += 1
+
+        ns += 1
+        #time.sleep(0.5)
+
+algorithm = (input("Which Scheduling algorithm would you like to simulate,type \n either SJF, Preemptive, or Round Robin only."))
+algorithm.lower
 
 if algorithm == "sjf":
     algorithm = "do something"
 elif algorithm == "preemptive":
     algorithm = "do something"
 elif (algorithm == "round robin" or algorithm == "roundrobin"):
-    t_quant = int(input("Indicate the time quantum "+str(i+1)+" (ns): "))
+    t_quant = int(input("Indicate the time quantum (ns): "))
+    mergeSort_arrivalTime(processList)
     roundRobin(processList, t_quant)
+    print("simulation done")
     
